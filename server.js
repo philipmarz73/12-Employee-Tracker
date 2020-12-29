b4fd16c9a99ea103af902ed6685e846fa96acb4f
Phlipper <marzullop73@gmail.com>
2020-12-29 16:18:38 -0500

    started setup of server.js, 'require' applications

---
+++ server.js
@@ -0,0 +1,11 @@
+const mysql = require("mysql")
+const inquirer = require("inquirer");
+const {response}= require("express");
+require("console.table");
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "MajiNo.1!",
+    database: ""
+})
