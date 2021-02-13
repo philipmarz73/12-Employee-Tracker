8ac772847308e8ff3cd22c8ca6ceca62ac94a14f
Phlipper <marzullop73@gmail.com>
2021-02-12 21:53:55 -0500

    added port 3306 to host

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const mysql = require("mysql")
 const inquirer = require("inquirer");
 const {response}= require("express");
 require("console.table");
+// requiring mySql, inquirer and express packages and console.table to 
+// test in Terminal 
+
 
 const db = mysql.createConnection({
     host: "localhost",
+    port: 3306
     user: "root",
     password: "MajiNo.1!",
     database: "employee_tracker",
